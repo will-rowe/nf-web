bbd26ea01db388546adeed7b40eb948775cf5cc9
Will Rowe <w.p.m.rowe@gmail.com>
2026-04-21 17:27:14 +0100

    perf: lazy-load fonts, lazy images; throttle background animation

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,24 @@
-import { StrictMode } from "react";
+import { StrictMode, useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "next-themes";
 import "./lib/i18n.ts";
 import "./index.css";
 import App from "./App.tsx";
 
+// Non-blocking font load: dynamically import heavy font after initial render
+function LoadFonts() {
+  useEffect(() => {
+    // Importing the font dynamically prevents render-blocking CSS.
+    import("@fontsource/audiowide/400.css").catch(() => {
+      /* ignore font load errors */
+    });
+  }, []);
+  return null;
+}
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <LoadFonts />
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <App />
     </ThemeProvider>
